d4f3068dec58fd0b5ec72c8968ff58a416b3f3c2
jjb213 <jeff.bachovchin@alumni.upenn.edu>
2015-01-23 10:39:05 -0500

    Header
    Added standard header

--- cachematrix.R
+++ cachematrix.R
@@ -1,3 +1,9 @@
+## R Programming (Coursera Data Scientist Signature Track)
+## Jeff Bachovchin
+## 23-01-2015
+##
+## Assignement #2
+
 ## Put comments here that give an overall description of what your
 ## functions do
 
